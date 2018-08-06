b5c08ac8d62c13b232d85fb35b039f1d4647f027
Sherezz Grant <rezziemaven@gmail.com>
2018-08-06 19:53:24 +0200

    fix: debugging backend

--- middleware/github.js
+++ middleware/github.js
@@ -7,11 +7,13 @@ require('../services/raven');
 module.exports = (req, res, next) => {
   const allowedUserAgent = 'GitHub-Hookshot/';
   const reqUserAgent = req.headers['user-agent'].match(/.+?\//gi).toString();
+  console.log("user agent",reqUserAgent);
 
   const requestSignature = req.headers['x-hub-signature'];
   let hmac = crypto.createHmac('sha1', keys.githubWebhookSecret);
   hmac.update(JSON.stringify(req.body));
   const calculatedSignature = 'sha1=' + hmac.digest('hex');
+  console.log("signature",calculatedSignature);
 
   if (reqUserAgent !== allowedUserAgent || requestSignature !== calculatedSignature) {
     Raven.captureException('Unable to store Pull Request!');
